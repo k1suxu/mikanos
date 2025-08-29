231c87ebbe79149d4c7c07217aab5399e15d4ab9
katsuki <katsuki.suzu03@gmail.com>
2025-08-30 00:53:32 +0900

    upd: utilsの拡充

--- utils/edk_build.sh
+++ utils/edk_build.sh
@@ -2,11 +2,15 @@
 
 CUR_PATH=$(pwd)
 EDK2_DIR="$HOME/edk2"
+KERNEL_PATH="$CUR_PATH/../kernel"
+
+# --- kernel.elfの生成 --- 
+cd "$KERNEL_PATH"
+make
 
 # --- QEMU を別ターミナルで起動 ---
 cd "$EDK2_DIR"
 source edksetup.sh
 build
 cd "$CUR_PATH"
-$HOME/osbook/devenv/run_qemu.sh $HOME/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi
-
+$HOME/osbook/devenv/run_qemu.sh $HOME/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi $KERNEL_PATH/kernel.elf
--- utils/send_to_usb.sh
+++ utils/send_to_usb.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+CUR_PATH=$(pwd)
+
 sudo mkdir -p /mnt/usbmem
 sudo mount -t drvfs D: /mnt/usbmem # D:の部分は適切なドライレターに変更する
 sudo mkdir -p /mnt/usbmem/EFI/BOOT
 sudo cp $HOME/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi /mnt/usbmem/EFI/BOOT/BOOTX64.EFI
+sudo cp $CUR_PATH/../kernel/kernel.elf /mnt/usbmem/kernel.elf
 sudo umount /mnt/usbmem
