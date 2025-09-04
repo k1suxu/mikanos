38e3c38e8fa9cee8de5a65bd1ff0bd23dc3265bc
katsuki <katsuki.suzu03@gmail.com>
2025-09-04 17:31:54 +0900

    day04c interfaceクラスを作成することで、Write内のif分を実質的にfor分の外にずらした

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -14,42 +14,84 @@ struct PixelColor {
 };
 
 /**
- * WritePixel write exactly one pixel of screen
- * @retval 0      success
- * @retval non-0  fail
+ * @class PixelWriter : ピクセル描画用の抽象クラス
  */
-int WritePixel(const FrameBufferConfig& config,
-               int x, int y, const PixelColor& c) {
-  const int pixel_position = config.pixels_per_scan_line * y + x;
-  if (config.pixel_format == kPixelRGBResv8BitPerColor) {
-    // 各ピクセルは8(red)+8(blue)+8(green)+8(offset for ABI)の計32バイトで構成されている => *4する
-    uint8_t* p = &config.frame_buffer[4 * pixel_position]; 
-    p[0] = c.r;
-    p[1] = c.g;
-    p[2] = c.b;
-  } else if (config.pixel_format == kPixelBGRResv8BitPerColor) {
-    uint8_t* p = &config.frame_buffer[4 * pixel_position]; 
-    p[0] = c.b;
-    p[1] = c.g;
-    p[2] = c.r;
-  } else {
-    return -1;
-  }
-  return 0;
+class PixelWriter {
+  public:
+    PixelWriter(const FrameBufferConfig& config) : config_{config} {}
+
+  // destructor
+  virtual ~PixelWriter() = default;
+  // 純粋仮想関数であるため、=0を指定
+  virtual void Write(int x, int y, const PixelColor& c) = 0;
+
+  protected:
+    uint8_t* PixelAt(int x, int y) {
+      return config_.frame_buffer + 4 * (config_.pixels_per_scan_line * y + x);
+    }
+
+  private:
+    const FrameBufferConfig& config_; 
+};
+
+class RGBResv8BitPerColorPixelWriter : public PixelWriter {
+  public:
+    using PixelWriter::PixelWriter;
+
+    // コンパイラにoverride修飾子をちゃんと伝える
+    virtual void Write(int x, int y, const PixelColor& c) override {
+      auto p = PixelAt(x, y);
+      p[0] = c.r;
+      p[1] = c.g;
+      p[2] = c.b;
+    }
+};
+
+class BGRResv8BitPerColorPixelWriter : public PixelWriter {
+  public:
+    using PixelWriter::PixelWriter;
+
+    virtual void Write(int x, int y, const PixelColor& c) override {
+      auto p = PixelAt(x, y);
+      p[0] = c.b;
+      p[1] = c.g;
+      p[2] = c.r;
+    }
+};
+
+// sizeはコンパイラから自動で渡されるらしい？？ [TODO]
+void* operator new(size_t size, void *buf) {
+  return buf;
 }
 
+// destructorで呼ばれるので便宜上定義しとく
+void operator delete(void *obj) noexcept {}
+
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+PixelWriter* pixel_writer;
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
+  switch (frame_buffer_config.pixel_format) {
+    case kPixelRGBResv8BitPerColor:
+      pixel_writer = new(pixel_writer_buf)
+        RGBResv8BitPerColorPixelWriter{frame_buffer_config};
+      break;
+    case kPixelBGRResv8BitPerColor:
+      pixel_writer = new(pixel_writer_buf)
+        BGRResv8BitPerColorPixelWriter{frame_buffer_config};
+      break;
+  }
+
   for (int x = 0; x < frame_buffer_config.horizontal_resolution; x++) {
     for (int y = 0; y < frame_buffer_config.vertical_resolution; y++) {
       // 一旦白塗り
-      WritePixel(frame_buffer_config, x, y, {255, 255, 255});
+      pixel_writer->Write(x, y, {255, 255, 255});
     }
   }
   for (int x = 0; x < 200; x++) {
     for (int y = 0; y < 100; y++) {
       // 緑の四角を描画
-      WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0});
+      pixel_writer->Write(x, y, {0, 255, 0});
     }
   }
   while (1) __asm__("hlt");
