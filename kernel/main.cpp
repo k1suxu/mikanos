41105f11545f5cb216d8901c55e55a53d8fa361a
katsuki <katsuki.suzu03@gmail.com>
2025-09-10 19:33:30 +0900

    day06a add mouse cursor and utility rectangle draw(fill) functions

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -13,10 +13,60 @@ void* operator new(size_t size, void* buf) {
 }
 
 // destructorで呼ばれるので便宜上定義しとく
-void operator delete(void* obj) noexcept {}
+void operator delete(void* obj) noexcept {
+
+}
+
+const PixelColor kDesktopBGColor{45, 118, 237}; // デスクトップ背景色
+const PixelColor kDesktopFGColor{255, 255, 255}; // カーソル色等
+
+const int kMouseCursorWidth = 15;
+const int kMouseCursorHeight = 24;
+const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
+  "@              ",
+  "@@             ",
+  "@.@            ",
+  "@..@           ",
+  "@...@          ",
+  "@....@         ",
+  "@.....@        ",
+  "@......@       ",
+  "@.......@      ",
+  "@........@     ",
+  "@.........@    ",
+  "@..........@   ",
+  "@...........@  ",
+  "@............@ ",
+  "@......@@@@@@@@",
+  "@......@       ",
+  "@....@@.@      ",
+  "@...@ @.@      ",
+  "@..@   @.@     ",
+  "@.@    @.@     ",
+  "@@      @.@    ",
+  "@       @.@    ",
+  "         @.@   ",
+  "         @@@   ",
+};
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
+
+char console_buf[sizeof(Console)];
+Console* console;
+
+int printk(const char* format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
@@ -30,25 +80,39 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       break;
   }
 
-  for (int x = 0; x < frame_buffer_config.horizontal_resolution; x++) {
-    for (int y = 0; y < frame_buffer_config.vertical_resolution; y++) {
-      // 一旦白塗り
-      pixel_writer->Write(x, y, {255, 255, 255});
-    }
-  }
-  for (int x = 0; x < 200; x++) {
-    for (int y = 0; y < 100; y++) {
-      // 緑の四角を描画
-      pixel_writer->Write(x, y, {0, 255, 0});
-    }
-  }
-  
-  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+  const int kFrameWidth = frame_buffer_config.horizontal_resolution;
+  const int kFrameHeight = frame_buffer_config.vertical_resolution;
 
-  char buf[128];
-  for (int i = 0; i < 27; i++) {
-    sprintf(buf, "line %d\n", i);
-    console.PutString(buf);
+  FillRectangle(*pixel_writer,
+                {0, 0},
+                {kFrameWidth, kFrameHeight - 50},
+                kDesktopBGColor);
+  FillRectangle(*pixel_writer,
+                {0, kFrameHeight - 50},
+                {kFrameWidth, 50},
+                {1, 8, 17});
+  FillRectangle(*pixel_writer,
+                {0, kFrameHeight - 50},
+                {kFrameWidth / 5, 50},
+                {80, 80, 80});
+  DrawRectangle(*pixel_writer,
+                {10, kFrameHeight - 40},
+                {30, 30},
+                {160, 160, 160});
+  
+  console = new(console_buf) Console{
+    *pixel_writer, kDesktopFGColor, kDesktopBGColor
+  };
+  printk("Welcome to MikanOS!\n");
+
+  for (int dy = 0; dy < kMouseCursorHeight; dy++) {
+    for (int dx = 0; dx < kMouseCursorWidth; dx++) {
+      if (mouse_cursor_shape[dy][dx] == '@') {
+        pixel_writer->Write(200 + dx, 100 + dy, {0, 0, 0});
+      } else if (mouse_cursor_shape[dy][dx] == '.') {
+        pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
+      }
+    }
   }
   
   while (1) __asm__("hlt");
