40f63e5c012474c68c8d8a8cb0990c57eb611c67
katsuki <katsuki.suzu03@gmail.com>
2025-10-08 22:04:44 +0900

    chore: fix indent

--- kernel/error.hpp
+++ kernel/error.hpp
@@ -3,30 +3,47 @@
 #include <array>
 
 class Error {
-  public:
-    enum Code {
-      kSuccess,
-      kFull,
-      kEmpty,
-      kLastOfCode,
-    };
+ public:
+  enum Code {
+    kSuccess,
+    kFull,
+    kEmpty,
+    kNoEnoughMemory,
+    kIndexOutOfRange,
+    kHostControllerNotHalted,
+    kInvalidSlotID,
+    kPortNotConnected,
+    kInvalidEndpointNumber,
+    kTransferRingNotSet,
+    kAlreadyAllocated,
+    kNotImplemented,
+    kInvalidDescriptor,
+    kBufferTooSmall,
+    kUnknownDevice,
+    kNoCorrespondingSetupStage,
+    kTransferFailed,
+    kInvalidPhase,
+    kUnknownXHCISpeedID,
+    kNoWaiter,
+    kLastOfCode,  // この列挙子は常に最後に配置する
+  };
 
-    Error(Code code) : code_{code} {}
+  Error(Code code) : code_{code} {}
 
-    operator bool() const {
-      return this->code_ != kSuccess;
-    }
+  operator bool() const {
+    return this->code_ != kSuccess;
+  }
 
-    const char* Name() const {
-      return code_names_[static_cast<int>(this->code_)];
-    }
-  
-  private:
-    static constexpr std::array<const char*, 3> code_names_ = {
-      "kSuccess",
-      "kFull",
-      "kEmpty",
-    };
+  const char* Name() const {
+    return code_names_[static_cast<int>(this->code_)];
+  }
 
-    Code code_;
+ private:
+  static constexpr std::array<const char*, 3> code_names_ = {
+    "kSuccess",
+    "kFull",
+    "kEmpty",
+  };
+
+  Code code_;
 };
